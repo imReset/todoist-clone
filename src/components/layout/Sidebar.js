0bd0cd9c5395d183e1f867def19d2e2c624ab39a
imReset <fffffreinds101@gmail.com>
2022-03-27 21:26:24 -0700

    commit

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar,
 } from "react-icons/fa";
+import { Projects } from "../Projects";
+import { useSelectedProjectValue } from "../../context";
 
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
@@ -36,7 +42,7 @@ export const Sidebar = () => {
         </span>
       </div>
       <ul className="sidebar__projects">Projects will be here!</ul>
-      Add project components here.
+      <Projects />
     </div>
   );
 };
