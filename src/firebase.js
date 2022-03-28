4748a2ab75a2c8933f252022394ea9c239d15fb3
imReset <fffffreinds101@gmail.com>
2022-03-27 19:40:00 -0700

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyC7fC0UlIcYIwV_xg9XGlO9crXZcup7qc8",
+  authDomain: "todoist-clone-fb8a0.firebaseapp.com",
+  databaseURL: "https://todoist-clone-fb8a0-default-rtdb.firebaseio.com",
+  projectId: "todoist-clone-fb8a0",
+  storageBucket: "todoist-clone-fb8a0.appspot.com",
+  messagingSenderId: "325124769842",
+  appId: "1:325124769842:web:c19a4f74c081da45c1942b",
+});
+
+export { firebaseConfig as firebase };
