6242438999c1e7af0a9808b405b8c9a776ed310f
imReset <fffffreinds101@gmail.com>
2022-03-28 22:39:42 -0700

    commit

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -17,28 +17,54 @@ export const Sidebar = () => {
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li data-testid="inbox" className="inbox">
+        <li
+          data-testid="inbox"
+          className={active === "inbox" ? "active" : undefined}
+          onClick={() => {
+            setActive("inbox");
+            setSelectedProject("INBOX");
+          }}
+        >
           <span>
             <FaInbox />
           </span>
           Inbox
         </li>
-        <li data-testid="inbox" className="today">
+        <li
+          data-testid="today"
+          className={active === "today" ? "active" : undefined}
+          onClick={() => {
+            setActive("today");
+            setSelectedProject("TODAY");
+          }}
+        >
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li data-testid="next_7" className="next_7">
+        <li
+          data-testid="next_7"
+          className={active === "next_7" ? "active" : undefined}
+          onClick={() => {
+            setActive("next_7");
+            setSelectedProject("NEXT_7");
+          }}
+        >
           <span>
             <FaRegCalendarAlt />
           </span>
           Next 7 Days
         </li>
       </ul>
-      <div className="sidebar__middle">
+      <div
+        className="sidebar__middle"
+        onClick={() => setShowProjects(!showProjects)}
+      >
         <span>
-          <FaChevronDown />
+          <FaChevronDown
+            className={!showProjects ? "hidden-projects" : undefined}
+          />
           <h2>Projects</h2>
         </span>
       </div>
